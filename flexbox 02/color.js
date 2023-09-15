3ffbbe765f43e4f4e8ce34c5a7e6b57afc5ea31e
MRicsi1211 <miklosrichard2002@gmail.com>
2023-09-15 08:21:39 +0200

    Color picker müködik

--- flexbox 02/color.js
+++ flexbox 02/color.js
@@ -1,6 +1,9 @@
+function changecolor(){
 let colorpicker = document.getElementById('colorpicker');
+
 
 setInterval(()=>{
   let color = colorpicker.value;
   document.body.style.backgroundColor = color;
 },200)
+}
